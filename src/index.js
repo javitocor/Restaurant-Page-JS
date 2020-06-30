ceb0a794c30216eab5a786984f0edd9873139628
javitocor <56809963+javitocor@users.noreply.github.com>
2020-06-30 16:00:04 +0200

    Created about page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { navbar } from './js/navbar.js';
+import { about } from './js/about.js';
 
 let nav = navbar();
+let aboutUs = about();
 
 nav.header();
+aboutUs.info();
 nav.footer();
---
+++ src/js/about.js
@@ -0,0 +1,43 @@
+const about = function () {
+    const content = document.getElementById('content');
+    
+    function info() {
+        const jumbo = document.createElement('div');
+        const container = document.createElement('div');
+        const header = document.createElement('h1');
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+
+        jumbo.classList.add('jumbotron','jumbotron-fluid');
+        container.classList.add("container");
+        header.classList.add("display-4");
+        p1.classList.add("lead");
+        p2.classList.add("lead");
+        p3.classList.add("lead");
+
+        header.innerHTML = "About Us";
+        p1.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae neque rutrum, lobortis quam eget, maximus urna. Maecenas congue felis ut tincidunt finibus. Duis tempus pretium mauris at tincidunt. Nullam sit amet quam vitae orci feugiat consequat. Sed mattis consequat eros, sed vestibulum ipsum dignissim facilisis. Curabitur ex felis, dictum a commodo sed, efficitur vitae tellus. Aenean pulvinar urna a turpis vulputate dapibus. Proin imperdiet at lacus vel cursus. Etiam convallis arcu sit amet dignissim bibendum. Aliquam ligula augue, mollis vitae orci nec, tristique pharetra justo. Morbi aliquet purus velit, quis porta ex tempus vel. Praesent euismod varius posuere. Vivamus sapien diam, ornare et odio nec, fermentum commodo risus.";
+        p2.innerHTML = "Aliquam at posuere mi, a fermentum urna. Aenean ac justo eget tortor venenatis luctus. Nam luctus elementum tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus eu tempor velit. Nulla ac volutpat orci, non condimentum sapien. Nunc in eros bibendum, posuere turpis vel, sollicitudin lacus.";
+        p3.innerHTML = "Phasellus nec orci ligula. Morbi commodo diam id mi convallis eleifend. Aenean eget turpis eu lorem feugiat facilisis. Vivamus sed ipsum eget risus tristique placerat vel non eros. Etiam eget sem ullamcorper, placerat arcu et, sagittis leo. Duis consectetur purus quis ipsum molestie finibus. Pellentesque erat odio, posuere at eleifend et, dapibus sit amet orci. Maecenas mollis dui eu vehicula luctus. Maecenas porta justo vel purus eleifend, auctor lobortis libero eleifend. Etiam faucibus pharetra ultrices. Cras ac leo et augue convallis tristique.";
+
+        container.append(header);
+        container.append(p1);
+        container.append(p2);
+        container.append(p3);
+
+        jumbo.append(container);
+        content.append(jumbo);
+
+    }
+    function render() {
+        info();
+    }
+
+    return {
+        info,
+        render,
+    };
+  };
+  
+  export { about };
