c86a5e5d15c22086cfd22bf901f3e0754f41f452
javitocor <56809963+javitocor@users.noreply.github.com>
2020-06-30 17:55:24 +0200

    Created skeleton for menu page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { navbar } from './js/navbar.js';
 import { about } from './js/about.js';
 import { location } from './js/location.js';
+import { menu } from './js/menu.js';
 
 let nav = navbar();
 let aboutUs = about();
 let loc = location();
+let eat = menu();
 
 nav.header();
+aboutUs.info();
 loc.restaurants();
+eat.food();
 nav.footer();
---
+++ src/js/menu.js
@@ -0,0 +1,13 @@
+const menu = function () {
+    const content = document.getElementById('content');
+    
+    function food() {
+
+    }
+
+    return {
+        food,
+    };
+  };
+  
+  export { menu };
